f93e66f199a279225bc2acc6e7d22bbe26b4d31d
Junyeong-An <2117ab@gmail.com>
2024-07-24 18:41:06 +0900

    refactor: header 로직 분리

--- src/main/java/roomescape/controller/TimeController.java
+++ src/main/java/roomescape/controller/TimeController.java
@@ -1,15 +1,20 @@
 package roomescape.controller;
 
+import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import roomescape.domain.Time;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import roomescape.dto.TimeDto;
 import roomescape.dto.TimeResDto;
 import roomescape.service.TimeService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/times")
@@ -24,10 +29,16 @@ public class TimeController {
     @ResponseBody
     public ResponseEntity<TimeResDto> addTime(@RequestBody TimeDto timeDto) {
         TimeResDto timeResDto = timeService.addTime(timeDto);
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/times/" + timeResDto.id());
+        HttpHeaders headers = createHeader("/times/" + timeResDto.id());
         return new ResponseEntity<>(timeResDto, headers, HttpStatus.CREATED);
     }
+
+    private HttpHeaders createHeader(String path) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", path);
+        return headers;
+    }
+
     @GetMapping
     public ResponseEntity<List<TimeResDto>> getAllTimes() {
         List<TimeResDto> times = timeService.getAllTimes();
